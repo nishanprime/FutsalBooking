0d77f39748504261d7341501dc1748fc11a631c5
nishanprime <thapanishan9@gmail.com>
2021-08-28 00:15:57 -0400

    Backend BoilerPlate

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,10 @@ const express = require("express");
 const app = express();
 
 app.get("/",(req,res)=>{
-    res.send("Welcome to futsal")
+    res.send("Welcome to BipinGayFutsal")
 })
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log("Server up and running");
+  console.log("Bipin is a gay");
 });
